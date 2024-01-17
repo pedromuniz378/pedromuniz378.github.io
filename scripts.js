633bc9cde242c4f71794741015064ffee7714577
pedromuniz378 <pedromvargas@hotmail.com>
2024-01-17 16:24:37 -0300

    Atualização para aceitar informações da Shopee

--- scripts.js
+++ scripts.js
@@ -4,16 +4,30 @@ function getParams() {
     console.log(params)
     var code = document.getElementById("code");
     if (params.get('code') != null) {
-        code.textContent = params.get('code');
+        code.textContent = 'Code: ' + params.get('code');
     } else {
-        code.textContent = 'Sem informações!';
+        code.textContent = 'Code: Sem informações!';
     }
-    
+
+    var shop_id = document.getElementById("shop_id");
+    if (params.get('shop_id') != null) {
+        shop_id.textContent = 'Shop_id: ' + params.get('shop_id');
+    } else {
+        shop_id.textContent = 'Shop_id: Sem informações!';
+    }
+
+    var main_account_id = document.getElementById("main_account_id");
+    if (params.get('main_account_id') != null) {
+        main_account_id.textContent = 'main_account_id: ' + params.get('main_account_id');
+    } else {
+        main_account_id.textContent = 'main_account_id: Sem informações!';
+    }
+
     var state = document.getElementById("state");
     if (params.get('state') != null) {
-        state.textContent = params.get('state');
+        state.textContent = 'state: ' + params.get('state');
     } else {
-        state.textContent = 'Sem informações!';
+        state.textContent = 'state: Sem informações!';
     }
 }
 
